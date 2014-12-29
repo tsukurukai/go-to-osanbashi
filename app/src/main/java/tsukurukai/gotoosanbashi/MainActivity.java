4c4aa9d0ad65110450d73cc06a812ef0d0d10d6e
satot <ph1.ph2.sa25@gmail.com>
2014-12-29 19:49:12 +0900

    Google Places API からスポット情報を取得する

--- app/src/main/java/tsukurukai/gotoosanbashi/MainActivity.java
+++ app/src/main/java/tsukurukai/gotoosanbashi/MainActivity.java
@@ -6,6 +6,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.Fragment;
@@ -19,6 +20,16 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +37,8 @@ import tsukurukai.gotoosanbashi.activities.MapsActivity;
 import tsukurukai.gotoosanbashi.fragments.LoadingDialogFragment;
 import tsukurukai.gotoosanbashi.models.CourseCalculator;
 import tsukurukai.gotoosanbashi.models.Spot;
+
+import static java.lang.Double.valueOf;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -88,39 +101,44 @@ public class MainActivity extends ActionBarActivity {
 
                     currentLocationExecute(new LocationListener() {
                         @Override
-                        public void onLocationChanged(Location location) {
-                            ArrayList<Spot> spots = new ArrayList<Spot>();
-                            spots.add(new Spot("ランドマークタワー", 35.454721, 139.631666));
-                            spots.add(new Spot("青葉台", 35.542955, 139.517182));
-                            spots.add(new Spot("十日市場", 35.526302, 139.516584));
-                            spots.add(new Spot("ズーラシア", 35.496483, 139.525851));
-                            spots.add(new Spot("山下公園", 35.445877, 139.649554));
-                            spots.add(new Spot("四季の森公園", 35.505727, 139.536819));
-                            spots.add(new Spot("八景島シーパラダイス", 35.337158, 139.645770));
+                        public void onLocationChanged(final Location location) {
 
-                            Spot start = new Spot("現在地", location.getLatitude(), location.getLongitude());
-                            Spot goal = new Spot("大桟橋", 35.451762, 139.647758);
-                            SharedPreferences sharedPreferences = getActivity().getSharedPreferences("data", getActivity().MODE_PRIVATE);
+                            new AsyncTask<Void, Void, Void>() {
+                                @Override
+                                protected Void doInBackground(Void... params) {
+                                    ArrayList<Spot> spots = getPlaces();
+                                    Spot start = new Spot("現在地", location.getLatitude(), location.getLongitude());
+                                    Spot goal = new Spot("大桟橋", 35.451762, 139.647758);
 
-                            for (int i = 0; i < 3; i++) {
-                                List<Spot> course = CourseCalculator.calculate(spots, 3, start, goal);
+                                    SharedPreferences sharedPreferences = getActivity().getSharedPreferences("data", getActivity().MODE_PRIVATE);
 
-                                for (int j = 0; j < course.size(); j++) {
-                                    sharedPreferences
+                                    for (int i = 0; i < 3; i++) {
+                                        List<Spot> course = CourseCalculator.calculate(spots, 3, start, goal);
+                                        for (int j = 0; j < course.size(); j++) {
+                                            sharedPreferences
                                             .edit()
                                             .putString("course:" + i + ":spots:" + j, course.get(j).toJson())
                                             .apply();
+                                        }
+                                        sharedPreferences
+                                            .edit()
+                                            .putInt("course:" + i + ":spotsCount", course.size())
+                                            .commit();
+                                    }
+
+                                    if (loadingDialogFragment != null && loadingDialogFragment.getDialog() != null) {
+                                        loadingDialogFragment.getDialog().dismiss();
+                                    }
+                                    return null;
                                 }
-                                sharedPreferences
-                                        .edit()
-                                        .putInt("course:" + i + ":spotsCount", course.size())
-                                        .commit();
-                            }
-                            if (loadingDialogFragment != null && loadingDialogFragment.getDialog() != null) {
-                                loadingDialogFragment.getDialog().dismiss();
-                            }
-                            Intent intent = MapsActivity.createIntent(getActivity());
-                            startActivity(intent);
+
+                                @Override
+                                protected void onPostExecute(Void aVoid) {
+                                    super.onPostExecute(aVoid);
+                                    Intent intent = MapsActivity.createIntent(getActivity());
+                                    startActivity(intent);
+                                }
+                            }.execute();
                         }
 
                         @Override
@@ -192,6 +210,43 @@ public class MainActivity extends ActionBarActivity {
                 }
             });
             return rootView;
+        }
+
+        private ArrayList<Spot> getPlaces() {
+            //String uri = "http://techbooster.org/feed/";
+            String baseUri  = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+            String key      = "key=" + Secret.GOOGLE_PLACES_API_KEY;
+            String location = "&location=35.454721,139.631666";
+            String types    = "&types=food";
+            String radius   = "&radius=500";
+            String sensor   = "&sensor=false";
+            String uri = baseUri + key + location + radius + types + sensor;
+            HttpGet request = new HttpGet(uri);
+            HttpResponse httpResponse;
+            HttpClient httpClient = new DefaultHttpClient();
+            ArrayList<Spot> spots = new ArrayList<Spot>();
+            try {
+                httpResponse = httpClient.execute(request);
+                int status = httpResponse.getStatusLine().getStatusCode();
+                if (HttpStatus.SC_OK == status) {
+                    JSONObject json = new JSONObject(EntityUtils.toString(httpResponse.getEntity()));
+                    JSONArray results = json.getJSONArray("results");
+                    for (int i = 0; i < results.length(); i++) {
+                        JSONObject jsonObject = results.getJSONObject(i);
+                        spots.add(new Spot(jsonObject.getString("name"),
+                                valueOf(jsonObject.getJSONObject("geometry").getJSONObject("location").getString("lat")),
+                                valueOf(jsonObject.getJSONObject("geometry").getJSONObject("location").getString("lng"))));
+                        Log.d("json_name", jsonObject.getString("name"));
+                        Log.d("json_latitude", jsonObject.getJSONObject("geometry").getJSONObject("location").getString("lat"));
+                        Log.d("json_longitude", jsonObject.getJSONObject("geometry").getJSONObject("location").getString("lng"));
+                    }
+                } else {
+                    Log.d("HttpStatus", "Status" + status);
+                }
+            } catch ( Exception e) {
+                throw new RuntimeException(e);
+            }
+            return spots;
         }
 
         private void currentLocationExecute(LocationListener listener) {
