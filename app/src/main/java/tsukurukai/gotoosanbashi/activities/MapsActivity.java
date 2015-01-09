c06243b48fb94f579559ea9623a2939e492c7e2a
trtraki <akio55252000@yahoo.co.jp>
2015-01-09 21:03:02 +0900

    コース保存時にメッセージを表示するように修正。

--- app/src/main/java/tsukurukai/gotoosanbashi/activities/MapsActivity.java
+++ app/src/main/java/tsukurukai/gotoosanbashi/activities/MapsActivity.java
@@ -5,10 +5,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -113,6 +113,8 @@ public class MapsActivity extends ActionBarActivity implements OnMapReadyCallbac
                         .edit()
                         .putInt("saved_course_count", ++savedCourseCount)
                         .commit();
+
+                Toast.makeText(v.getContext(), "コース" + ++selectedCourse + "を保存しました", Toast.LENGTH_SHORT).show();
             }
         });
     }
