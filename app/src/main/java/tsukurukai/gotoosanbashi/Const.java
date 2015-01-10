398f03a6ab241b273538f975ecc4634d90faf78b
satot <ph1.ph2.sa25@gmail.com>
2015-01-10 15:09:58 +0900

    スポットはレストランを抜く。（satot）

--- app/src/main/java/tsukurukai/gotoosanbashi/Const.java
+++ app/src/main/java/tsukurukai/gotoosanbashi/Const.java
@@ -7,4 +7,8 @@ public class Const {
     public static final String GOAL_NAME = "大桟橋";
     public static final Double GOAL_LATITUDE  = 35.451762;
     public static final Double GOAL_LONGITUDE = 139.647758;
+    public static final String SPOT_TYPE_WHITE_LIST = "amusement_park|aquarium|art_gallery" +
+            "|bowling_alley|church|hindu_temple|library|mosque|museum|park|place_of_worship" +
+            "|shopping_mall|spa|synagogue|university|zoo";
+    public static final String SPOT_TYPE_BLACK_LIST = "bakery|cafe|convenience_store|food|restaurant";
 }
