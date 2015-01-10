ab3c794a9f437840ba470b680c3387a86d751dd8
trtraki <akio55252000@yahoo.co.jp>
2015-01-10 20:51:39 +0900

    メニューの戻る処理を追加。

--- app/src/main/java/tsukurukai/gotoosanbashi/activities/HistoryListActivity.java
+++ app/src/main/java/tsukurukai/gotoosanbashi/activities/HistoryListActivity.java
@@ -4,13 +4,11 @@ import android.app.ListFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -47,6 +45,22 @@ public class HistoryListActivity extends ActionBarActivity {
                     .add(R.id.container, new CourseHistoryFragment())
                     .commit();
         }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
