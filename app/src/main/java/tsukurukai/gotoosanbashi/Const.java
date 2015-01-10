fd1c0f5404d55188f2a4cbc91fcf70d32b7615c2
satot <ph1.ph2.sa25@gmail.com>
2015-01-10 15:13:35 +0900

    大さん橋を正しい表記に修正

--- app/src/main/java/tsukurukai/gotoosanbashi/Const.java
+++ app/src/main/java/tsukurukai/gotoosanbashi/Const.java
@@ -4,7 +4,7 @@ package tsukurukai.gotoosanbashi;
  * Created by satot on 2014/12/29.
  */
 public class Const {
-    public static final String GOAL_NAME = "大桟橋";
+    public static final String GOAL_NAME = "大さん橋";
     public static final Double GOAL_LATITUDE  = 35.451762;
     public static final Double GOAL_LONGITUDE = 139.647758;
     public static final String SPOT_TYPE_WHITE_LIST = "amusement_park|aquarium|art_gallery" +
